207d71611e76c154add365a43b9f540fcc196906
Fiboliriodem <felipe123654_vcardoso@outlook.com>
2023-11-29 23:04:50 -0300

    modified:   JDBC-MySql-main/JDBC-MySql-main/src/main/java/poo/Main.java 	modified:   JDBC-MySql-main/JDBC-MySql-main/target/classes/poo/Main.class

--- JDBC-MySql-main/JDBC-MySql-main/src/main/java/poo/Main.java
+++ JDBC-MySql-main/JDBC-MySql-main/src/main/java/poo/Main.java
@@ -2,13 +2,14 @@ package poo;
 
 import poo.dao.CharacterDAO;
 import poo.model.Character;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
         CharacterDAO characterDAO = new CharacterDAO();
 
-        int escolha = 0;
+        
 
         System.out.println("bem vindo usuário, o que você deseja hoje?\n\n");
         System.out.println("1. Criar\n");
@@ -16,6 +17,8 @@ public class Main {
         System.out.println("3. Listar\n");
         System.out.println("4. Deletar\n");
         System.out.println("5. Filtar personagem\n");
+        Scanner choice = new Scanner(System.in);
+        int escolha = choice.nextInt();
 
         
         switch (escolha) {
@@ -26,7 +29,7 @@ public class Main {
                 
                 break;
             case 3:
-                
+              characterDAO.listar();  
                 break;
             case 4:
                 
@@ -39,9 +42,6 @@ public class Main {
                 break;
         }
         
-
-
-        characterDAO.listar();
 
     }
 }
